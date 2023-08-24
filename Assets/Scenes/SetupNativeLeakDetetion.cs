1fc5dea5e15094dd1d96937dfba31c7ad8e1f635
ringopie27 <131889202+ringopie27@users.noreply.github.com>
2023-08-24 13:10:16 +0900

    SetupNativeLeakDetectionの設定

---
+++ Assets/Scenes/SetupNativeLeakDetetion.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Unity.Collections;
+
+public class SetupNativeLeakDetection : MonoBehaviour
+{
+    void Start()
+    {
+        NativeLeakDetection.Mode = NativeLeakDetectionMode.EnabledWithStackTrace;
+    }
+}
+

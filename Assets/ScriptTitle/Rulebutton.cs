32343d3a1ea7bb7b4fb89b8b2cd9e68fb1abb4ce
ntkokd <natsunatsushokora@gmail.com>
2023-08-24 21:47:38 +0900

    タイトルからルールに飛ぶ

---
+++ Assets/ScriptTitle/Rulebutton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Rulebutton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ボタンを押した時の処理
+    public void Click()
+    {
+        //ログ出力
+        Debug.Log("押したよ！");
+        SceneManager.LoadScene("Rule");
+    }
+}
